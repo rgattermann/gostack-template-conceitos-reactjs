acf5f29dac8cd9af3dfa7888d612bb95c442ce32
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2021-07-08 08:47:29 -0300

    Implementing react interface repositories api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+      api.get('repositories').then(response => {
+        setRepositories(response.data);
+      });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Front-end com React Native',
+      url: 'https://github.com/rgattermann/gostack-template-conceitos-reactjs',
+      techs: ['Nodejs', 'ReactJS', 'PHP']
+    });
+
+    const repo = response.data;
+    setRepositories([...repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    console.log(response.status);
+
+    if (response.status === 204) {
+        const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+        if (repoIndex >= 0) {
+          repositories.splice(repoIndex, 1);
+
+          setRepositories([...repositories]);
+        }
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
